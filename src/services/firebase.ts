4b0130595d33007580f4c84373646690cf4b4d58
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-10-20 00:09:54 -0300

    ajuste firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,16 @@
-import firebase from 'firebase/app'
-
-import 'firebase/auth'
-import 'firebase/database'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBgH3T-a5UG_-_NaHAdPGGj37MuZLjLdHk",
-  authDomain: "letmeask-4708f.firebaseapp.com",
-  databaseURL: "https://letmeask-4708f-default-rtdb.firebaseio.com",
-  projectId: "letmeask-4708f",
-  storageBucket: "letmeask-4708f.appspot.com",
-  messagingSenderId: "1077469852519",
-  appId: "1:1077469852519:web:1a1ddb059981271ecbf5c2",
-  measurementId: "G-DETYQRMTGP"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig)
