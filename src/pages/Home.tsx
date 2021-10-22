abeb3783c17b99aae13835b8f924936c5e92c3fc
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-10-21 23:01:17 -0300

    criando componente botão e os context

---
+++ src/pages/Home.tsx
@@ -0,0 +1,48 @@
+import { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { auth, firebase } from '../services/firebase'
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIcon from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
+import { Button } from '../components/Button'
+
+export function Home(){
+
+    const history = useHistory()
+   
+    function handleCreateRoom(){
+
+            history.push('/rooms/new')
+        })    
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"></img>
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as dúvidas de sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="LetmeAsk"></img>
+                    <button onClick={handleCreateRoom} className="create-room">
+                        <img src={googleIcon} alt="Logo do Google"></img>
+                        Crie sua sala com o Google
+                    </button>
+                    <div className="separator">ou entre em uma sala</div>
+                    <form>
+                        <input 
+                            type="text" 
+                            placeholder="Digite o código da sala"
+                        />
+                        <Button type="submit">
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
