c44d4f3bd32d31ae3bf6987c89dbf9002dee5696
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-10-24 19:07:05 -0300

    finalizando  projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,24 @@
-import { createContext, useState } from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
-import { Home } from './pages/Home';
-import { NewRoom } from './pages/NewRoom';
-import { firebase, auth} from './services/firebase'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import './styles/global.scss'
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
-type User ={
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType ={
-  user: User | undefined;
-  signInWithGoogle: () => void;
-}
-
-export const AuthContext = createContext({} as AuthContextType)
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
-
-  const [user, setUser] = useState<User>()
-  
-  function signInWithGoogle(){
-    const provider = new firebase.auth.GoogleAuthProvider()
-    
-    auth.signInWithPopup(provider).then(result =>{
-      if(result.user){
-        const { displayName, photoURL, uid} = result.user
-
-        if(!displayName || !photoURL){
-          throw new Error('Missing Information from Google Account')
-        }
-
-        setUser({
-          id: uid,
-          name: displayName,
-          avatar: photoURL
-        })
-      }
-    })  
-  }
-
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, signInWithGoogle }}>
-        <Route path="/" exact component={Home}/>
-        <Route path="/rooms/new" component={NewRoom}/>
-      </AuthContext.Provider>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,21 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/database';
+import firebase from 'firebase/app';
+
+import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth()
+const auth = firebase.auth();
 const database = firebase.database();
 
 export { firebase, auth, database }
